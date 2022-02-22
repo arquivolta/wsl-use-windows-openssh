3ed527f235f8382e6946aa4d916e05f27576d5a2
Anaïs Betts <anais@anaisbetts.org>
2022-02-22 17:05:30 -0500

    Set up a profile.d script

---
+++ etc/profile.d/90-enable-wsl-ssh.sh
@@ -0,0 +1,12 @@
+# Check to make sure we've set up ssh-agent
+if ! sc.exe query ssh-agent | grep 'RUNNING' > /dev/null; then
+	echo "The Windows OpenSSH Agent isn't running, start it via 'wsl-enable-ssh-agent'" >&2
+fi
+
+## NB: We can't find PREFIX easily in a profile.d script so we'll need to
+## try to guess what it is by running down the existing path
+BINDIR=$(which wsl-enable-ssh-agent)
+PREFIX=$(realpath "$BINDIR/..")
+
+## Put our symlinked OpenSSH in front of the real one
+export PATH="$PREFIX/lib/wsl-ssh:$PATH"
