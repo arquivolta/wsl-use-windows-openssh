7528483c1615bcf11fbc57e2e3b5929adb98bd5a
Anaïs Betts <anais@anaisbetts.org>
2022-02-22 17:42:31 -0500

    Fix dumb mistakes

--- etc/profile.d/90-enable-wsl-ssh.sh
+++ etc/profile.d/90-enable-wsl-ssh.sh
@@ -5,7 +5,7 @@ fi
 
 ## NB: We can't find PREFIX easily in a profile.d script so we'll need to
 ## try to guess what it is by running down the existing path
-BINDIR=$(which wsl-enable-ssh-agent)
+BINDIR=$(dirname `which wsl-enable-ssh-agent`)
 PREFIX=$(realpath "$BINDIR/..")
 
 ## Put our symlinked OpenSSH in front of the real one
