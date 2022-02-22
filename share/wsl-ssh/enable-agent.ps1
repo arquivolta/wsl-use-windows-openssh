0c806baac14a8e13b7bc3fc2d893b0b6c85b77cd
Anaïs Betts <anais@anaisbetts.org>
2022-02-22 16:56:34 -0500

    Write WSL app to enable Windows SSH agent

---
+++ share/wsl-ssh/enable-agent.ps1
@@ -0,0 +1,25 @@
+param([switch]$Elevated)
+
+function Test-Admin {
+    $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+    $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+}
+
+if ((Test-Admin) -eq $false)  {
+    if ($elevated) {
+        # tried to elevate, did not work, aborting
+    } else {
+        Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
+    }
+    exit
+}
+
+try {
+	Add-WindowsCapability -Online -Name OpenSSH.Client~~~~0.0.1.0
+	Set-Service -Name ssh-agent -StartupType Automatic
+	Start-Service -Name ssh-agent
+} catch {
+  Write-Output $_
+}
+
+exit
