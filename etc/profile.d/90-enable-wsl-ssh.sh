0e84ab46233d5444e0f0d08adea0ddb03be8fd70
Fabian Rensing <61296674+frensing@users.noreply.github.com>
2022-10-04 10:29:39 +0200

    Fix nvm problem with same variable name

--- etc/profile.d/90-enable-wsl-ssh.sh
+++ etc/profile.d/90-enable-wsl-ssh.sh
@@ -6,7 +6,7 @@ fi
 ## NB: We can't find PREFIX easily in a profile.d script so we'll need to
 ## try to guess what it is by running down the existing path
 BINDIR=$(dirname `which wsl-enable-ssh-agent`)
-PREFIX=$(realpath "$BINDIR/..")
+PREFIXX=$(realpath "$BINDIR/..")
 
 ## Put our symlinked OpenSSH in front of the real one
-export PATH="$PREFIX/lib/wsl-ssh:$PATH"
+export PATH="$PREFIXX/lib/wsl-ssh:$PATH"
